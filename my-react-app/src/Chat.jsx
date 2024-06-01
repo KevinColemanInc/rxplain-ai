12e33915e38a1edd91179a662d606c856bfce529
njokafor <ok4nnaemeka@gmail.com>
2024-06-01 12:49:51 -0700

    Added template components

---
+++ my-react-app/src/Chat.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function Chat() {
+
+  return (
+    <>
+      <div>
+      </div>
+    </>
+  )
+}
+
+export default Chat
---
+++ my-react-app/src/ChatArea.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function ChatArea() {
+
+  return (
+    <>
+      <div>
+      </div>
+    </>
+  )
+}
+
+export default ChatArea
